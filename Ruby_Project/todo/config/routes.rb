304cab54499a2326ec8d22859c5b361bc10ccb1f
vanceleon <vanceleon44@gmail.com>
2019-04-16 12:50:30 -0400

    rendering items in todo list moved files into todo_items added methods to todo_items_controller

--- Ruby_Project/todo/config/routes.rb
+++ Ruby_Project/todo/config/routes.rb
@@ -4,8 +4,8 @@ Rails.application.routes.draw do
     member do 
       patch :complete
     end
+  # end
   end
-end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "todo_lists#index"
 end
