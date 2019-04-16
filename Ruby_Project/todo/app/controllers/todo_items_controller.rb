09950d031c316300b2963056df10873d293eacf3
vanceleon <vanceleon44@gmail.com>
2019-04-16 10:48:56 -0400

    mark complete functions

--- Ruby_Project/todo/app/controllers/todo_items_controller.rb
+++ Ruby_Project/todo/app/controllers/todo_items_controller.rb
@@ -1,21 +1,13 @@
 class TodoItemsController < ApplicationController
     before_action :set_todo_list
+    before_action :set_todo_item, except: [:create]
 
     def create
         @todo_item = @todo_list.todo_items.create(todo_item_params)
         redirect_to @todo_list
     end
 
-    private
-
-    def set_todo_list
-        @todo_list = TodoList.find(params[:todo_list_id])
-    end
-
-    def todo_item_params
-        params [:todo_item].permit(:content)
-
-    end
+    
 
     def destroy 
         @todo_item = @todo_list.todo.items.find(params[:id])
@@ -26,7 +18,28 @@ class TodoItemsController < ApplicationController
         end
         redirect_to @todo_list
     end
-    
+
+    def complete
+        @todo_item.update_attribute(:complete_at, Time.now)
+        redirect_to @todo_list, notice: "Todo item completed"
+    end
+
+    private
+
+    def set_todo_list
+        @todo_list = TodoList.find(params[:todo_list_id])
+    end
+
+    def todo_item_params
+        params [:todo_item].permit(:content)
+    end
+
+    def set_todo_item 
+        @todo_item = @todo_list.todo_items.find(params[:id])
+    end
+
+
+
 
 end
 
