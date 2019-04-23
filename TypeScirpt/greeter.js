2e782db1d6bcaf2dc2c4b2851fa9157d6c6512e3
vanceleon <vanceleon44@gmail.com>
2019-04-23 19:26:59 -0400

    added class implementation of typescript

--- TypeScirpt/greeter.js
+++ TypeScirpt/greeter.js
@@ -1,41 +1,27 @@
 // function greeter(person: string) {
 //     return "Hello, " + person;
 // }
-
 // let user = "Vance Leon";
-
 // document.body.innerHTML = greeter(user);
-
-
-
 // interface Person {
 //     firstName: string;
 //     lastName: string;
 // }
-
 // function greeter(person: Person) {
 //     return "Hello, " + person.firstName + " " + person.lastName;
 // }
-
 // let user = {firstName: "Jane", lastName: "User"};
-
 // document.body.innerHTML = greeter(user);
-
-
 // CLass Implementation
-class Student {
-    fullName: string;
-    constructor(public firstName: string, public middleInitial: string, public lastName: string) {
+var Student = /** @class */ (function () {
+    function Student(firstName, middleInitial, lastName) {
+        this.firstName = firstName;
+        this.middleInitial = middleInitial;
+        this.lastName = lastName;
         this.fullName = firstName + " " + middleInitial + " " + lastName;
     }
-}
-
-interface Person {
-    firstName: string;
-    lastName: string;
-
-}
-
-function greeter(person: Person) {
+    return Student;
+}());
+function greeter(person) {
     return "Hello, " + person.firstName + " " + person.lastName;
 }
