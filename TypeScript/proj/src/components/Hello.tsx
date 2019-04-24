de5358e3f6af899c867a579ad716ccb75e3df8b6
vanceleon <vanceleon44@gmail.com>
2019-04-23 20:06:17 -0400

    class component

---
+++ TypeScript/proj/src/components/Hello.tsx
@@ -0,0 +1,5 @@
+// import * as React from "react";
+
+// export interface HelloProps { complier: string; framework: string; }
+
+// export const Hello = ( props: HelloProps ) => <h1>Hello from {props.complier} and {props.framework}!</h1>;
